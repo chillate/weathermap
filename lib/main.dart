a05abfced261ec32db364e616fa11f85b260109b
chillate <chillate-app@proton.me>
2023-11-29 11:23:16 +0100

    init

---
+++ lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather API Map',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple)
+            .copyWith(secondary: Colors.deepPurple),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late double latitude = 47.47; // Latitude
+  late double longitude = -0.56; // Longitude
+  final apiKey = 'c0bbff4a824cce23670fa594dfa7e8b1';
+
+  String weatherDescription = '';
+  double temperature = 0.0;
+  String weatherIcon = 'assets/default.png';
+
+
+  Map<String, String> infos = {};
+
+
+  @override
+  void initState() {
+    super.initState();
+    fetchWeatherData();
+  }
+
+  Future<void> fetchWeatherData() async {
+    final url = 'https://api.openweathermap.org/data/2.5/onecall?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric';
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      Map<String, dynamic> currentData = data['current']; 
+
+
+      setState(() {
+        weatherDescription = data['current']['weather'][0]['main'];
+        temperature = data['current']['temp'];
+        weatherIcon = getWeatherIcon(weatherDescription);
+
+        print('Weather: $weatherDescription');
+        print('Temperature: $temperature°C');
+
+        infos = {
+          'Date/Time': DateTime.fromMillisecondsSinceEpoch(currentData['dt'] * 1000).toString(),
+          'Sunrise': currentData['sunrise'].toString(),
+          'Sunset': currentData['sunset'].toString(),
+          'Temperature': currentData['temp'].toString() + ' K',
+          'Feels Like': currentData['feels_like'].toString(),
+          'Pressure': currentData['pressure'].toString(),
+          'Humidity': currentData['humidity'].toString(),
+          'Dew Point': currentData['dew_point'].toString(),
+          'UV Index': currentData['uvi'].toString(),
+          'Clouds': currentData['clouds'].toString(),
+          'Visibility': currentData['visibility'].toString(),
+          'Wind Speed': currentData['wind_speed'].toString(),
+          'Wind Degree': currentData['wind_deg'].toString(),
+          'Wind Gust': currentData['wind_gust'].toString(),
+        };
+      });
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+
+  String getWeatherIcon(String description) {
+    if (description.toLowerCase().contains('clear')) {
+      return 'assets/sun.jpg';
+    } else if (description.toLowerCase().contains('clouds')) {
+      return 'assets/cloudy.png';
+    } else if (description.toLowerCase().contains('rain')) {
+      return 'assets/rain.jpg';
+    } else {
+      return 'assets/default.png';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Weather App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              weatherIcon,
+              width: 50,
+              height: 50,
+            ),
+            Text('Weather: $weatherDescription'),
+            Text('Temperature: $temperature°C'),
+            Column(
+              children: infos.entries.map((entry) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Text(entry.key + ": ", style: TextStyle(fontWeight: FontWeight.bold)),
+                      Text(entry.value),
+                    ],
+                    ),
+                  );
+              }).toList(),
+            ),
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
